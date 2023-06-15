6188eaa8f9b1e072cd9348f70f0f112d6bc22bd9
AmirHo3ein <amirhosein2006.n@gmail.com>
2023-06-16 01:12:09 +0430

    completed main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ path = f"C:{os.getenv('HOMEPATH')}\Desktop\\"
 dir_name = 'tmp'
 
 try:
+    os.rmdir(path + dir_name)
     os.mkdir(path + dir_name)
-except FileExistsError:
-    print(f"'{dir_name}' already exists.")
+except FileNotFoundError:
+    ...
